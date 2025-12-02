ffc7a3d3f6d542cac1b2bf62ef4aefde40e62c11
azizhadiid <azizalhadiid88@gmail.com>
2025-12-02 18:21:57 +0700

    Section Testimonial is Done

--- resources/views/penyewa/beranda.blade.php
+++ resources/views/penyewa/beranda.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.layoutPenyewa')
 
-@section('title', 'KosKita - Temukan Kos Impianmu')
+@section('title', 'SiKos - Temukan Kos Impianmu')
 
 @section('konten')
 <!-- Hero Section -->
@@ -38,7 +38,7 @@
 <section class="features-section" id="features">
     <div class="container">
         <div class="section-title">
-            <h2>Kenapa Pilih KosKita?</h2>
+            <h2>Kenapa Pilih SiKos?</h2>
             <p>Kemudahan dan kenyamanan dalam satu platform</p>
         </div>
         <div class="row g-4">
@@ -170,6 +170,78 @@
                         </div>
                         <div class="kos-price">
                             Rp 1.5jt <span>/ bulan</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Testimonial Section -->
+<section class="testimonial-section" id="testimoni">
+    <div class="container">
+        <div class="section-title">
+            <h2>Apa Kata Mereka?</h2>
+            <p>Cerita dari mahasiswa yang sudah menemukan kos impiannya</p>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-4 col-md-6">
+                <div class="testimonial-card">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p class="testimonial-text">"Platform yang sangat membantu! Dalam 2 hari aja udah nemu kos yang pas
+                        banget sama budget dan lokasi yang aku mau. Prosesnya juga gampang banget!"</p>
+                    <div class="testimonial-author">
+                        <img src="https://i.pravatar.cc/100?img=1" alt="User 1">
+                        <div>
+                            <h5>Sarah Aulia</h5>
+                            <p>Mahasiswa UGM</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="testimonial-card">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p class="testimonial-text">"Sebagai mahasiswa baru yang pindah kota, SiKos beneran life saver!
+                        Fasilitasnya lengkap, harganya jelas, dan owner kosnya juga responsif. Recommended!"</p>
+                    <div class="testimonial-author">
+                        <img src="https://i.pravatar.cc/100?img=13" alt="User 2">
+                        <div>
+                            <h5>Budi Santoso</h5>
+                            <p>Mahasiswa ITB</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="testimonial-card">
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                    <p class="testimonial-text">"Fitur filternya super helpful! Bisa cari kos sesuai kebutuhan, dari
+                        WiFi sampe parkiran motor. Pokoknya cocok banget buat mahasiswa modern kayak kita!"</p>
+                    <div class="testimonial-author">
+                        <img src="https://i.pravatar.cc/100?img=5" alt="User 3">
+                        <div>
+                            <h5>Dina Maharani</h5>
+                            <p>Mahasiswa UNAIR</p>
                         </div>
                     </div>
                 </div>
